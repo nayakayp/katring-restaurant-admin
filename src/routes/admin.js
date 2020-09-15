dc6c1643cae2338d923626dc1a3721ce41e4c1fe
nayakayp <nayakayp@gmail.com>
2020-09-15 11:26:38 +0700

    final code for admin design

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -2,23 +2,34 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/editprofil", (req, res) => {
-  res.render("editprofil.ejs", {});
+  res.render("editprofil.ejs", { pageName: "editprofil", layout: "layout" });
 });
 
 router.get("/", (req, res) => {
-  res.render("dashboard.ejs", { pageName: "dashboard" });
+  res.render("dashboard.ejs", { pageName: "dashboard", layout: "layout" });
 });
 
 router.get("/transaksi", (req, res) => {
-  res.render("transaksi.ejs", { pageName: "transaksi" });
+  res.render("transaksi.ejs", { pageName: "transaksi", layout: "layout" });
+});
+
+router.get("/transaksi/transaksi-detail", (req, res) => {
+  res.render("transaksi-detail.ejs", {
+    pageName: "transaksi",
+    layout: "layouts/detail",
+  });
 });
 
 router.get("/produk", (req, res) => {
-  res.render("produk.ejs", {});
+  res.render("produk.ejs", { pageName: "produk", layout: "layout" });
 });
 
 router.get("/pengguna", (req, res) => {
-  res.render("pengguna.ejs", {});
+  res.render("pengguna.ejs", { pageName: "pengguna", layout: "layout" });
+});
+
+router.get("/login", (req, res) => {
+  res.render("login.ejs", { layout: "layouts/entrance" });
 });
 
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -11,6 +11,8 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(expressLayouts);
+app.set("layout", "layouts/entrance");
+app.set("layout", "layouts/detail");
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
